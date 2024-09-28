2b65f9273b26bb0e9466d3304bf344648df614f5
varun <varunteja007006@gmail.com>
2024-09-28 16:05:16 +0530

    Added swapy layout

--- components/root/home/swapy.tsx
+++ components/root/home/swapy.tsx
@@ -1,20 +1,20 @@
 "use client";
 import { useEffect } from "react";
-import "./style.css";
 import { createSwapy } from "@/lib/swapy";
+import WeatherComp from "./weather";
+import SearchBox from "./search-box";
+import TempGraph from "../graphs/temp-graph";
 
 const DEFAULT = {
   "1": "a",
   "3": "c",
   "4": "d",
-  "2": null,
 };
 
 function A() {
   return (
     <div className="item a" data-swapy-item="a">
-      <div className="handle" data-swapy-handle></div>
-      <div>A</div>
+      <WeatherComp />
     </div>
   );
 }
@@ -22,7 +22,7 @@ function A() {
 function C() {
   return (
     <div className="item c" data-swapy-item="c">
-      <div>C</div>
+      <SearchBox />
     </div>
   );
 }
@@ -30,7 +30,7 @@ function C() {
 function D() {
   return (
     <div className="item d" data-swapy-item="d">
-      <div>D</div>
+      <TempGraph />
     </div>
   );
 }
@@ -48,27 +48,20 @@ function getItemById(itemId: "a" | "c" | "d" | null) {
 
 export default function Swapy() {
   const slotItems: Record<string, "a" | "c" | "d" | null> =
-    localStorage.getItem("slotItem")
+    window?.localStorage !== undefined && localStorage.getItem("slotItem")
       ? JSON.parse(localStorage.getItem("slotItem")!)
       : DEFAULT;
 
   useEffect(() => {
     const container = document.querySelector(".container")!;
+
     const swapy = createSwapy(container, {
       swapMode: "hover",
       continuousMode: false,
     });
+
     swapy.onSwap(({ data }) => {
-      console.log("swap", data);
       localStorage.setItem("slotItem", JSON.stringify(data.object));
-    });
-
-    swapy.onSwapEnd(({ data }) => {
-      console.log("end", data);
-    });
-
-    swapy.onSwapStart(() => {
-      console.log("start");
     });
 
     return () => {
@@ -77,20 +70,13 @@ export default function Swapy() {
   }, []);
 
   return (
-    <div className="container">
-      <div className="slot a" data-swapy-slot="1">
-        {getItemById(slotItems["1"])}
-      </div>
-      <div className="second-row">
-        <div className="slot b" data-swapy-slot="2">
-          {getItemById(slotItems["2"])}
+    <div className="container grid grid-cols-1 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-start gap-5">
+        <div data-swapy-slot="1">{getItemById(slotItems["1"])}</div>
+        <div className="space-y-2">
+          <div data-swapy-slot="3">{getItemById(slotItems["3"])}</div>
+          <div data-swapy-slot="4">{getItemById(slotItems["4"])}</div>
         </div>
-        <div className="slot c" data-swapy-slot="3">
-          {getItemById(slotItems["3"])}
-        </div>
-      </div>
-      <div className="slot d" data-swapy-slot="4">
-        {getItemById(slotItems["4"])}
       </div>
     </div>
   );
